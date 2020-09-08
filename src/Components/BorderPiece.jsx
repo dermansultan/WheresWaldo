40a93d8dfb217b2fc226ae34d7cdd8600294382e
dermansultan <abdurrehmansultan4@gmail.com>
2020-09-07 23:40:53 -0400

    bordercolor change on character select

--- src/Components/BorderPiece.jsx
+++ src/Components/BorderPiece.jsx
@@ -1,25 +1,40 @@
 import React from "react";
 
-function BorderPiece(props){
-    const style = {
-        left: `${props.left - 30 / 2}px`,
-        top: `${props.top - 50 / 2}px`,
-        position: 'absolute',
-        display: `${props.clicked}`,
-        justifycontent: 'flexstart',
-        flexdirection: 'row',
-    }
-    return(
-        <div className='borderContainer' style={style}>
-                <div className='border'>
-                </div>
-                <select className='charSelect' value={props.value} onChange={props.onChange} name='selectedChar'>
-                    <option value='waldo'>Waldo</option>
-                    <option value='wizard'>Wizard</option>
-                    <option value='odlaw'>Odlaw</option>
-                </select>
-            </div>
-    )
+function BorderPiece(props) {
+  const containerStyle = {
+    position: "absolute",
+    justifyContent: "flexstart",
+    flexDirection: "row",  
+    left: `${props.left - 30 / 2}px`,
+    top: `${props.top - 50 / 2}px`,
+    display: `${props.clicked}`,
+  };
+
+  const borderStyle = {
+    borderWidth: "2px",
+    borderStyle: "dashed",
+    height: "50px",
+    width: "30px",
+    pointerEvents: "none",
+    borderColor: `${props.color}`
+  }
+
+
+  return (
+    <div className="borderContainer" style={containerStyle}>
+      <div className="border" style={borderStyle}></div>
+      <select
+        className="charSelect"
+        value={props.value}
+        onChange={props.onChange}
+        name="selectedChar"
+      >
+        {props.characters.waldo ? <option value="waldo" disabled>Waldo</option> : <option value="waldo">Waldo</option>}
+        {props.characters.odlaw ? <option value="odlaw" disabled>Odlaw</option> : <option value="odlaw">Odlaw</option>}
+        {props.characters.wizard ? <option value="wizard" disabled>Wizard</option> : <option value="wizard">Wizard</option>}
+      </select>
+    </div>
+  );
 }
 
-export default BorderPiece
+export default BorderPiece;
--- src/Components/GameStart.jsx
+++ src/Components/GameStart.jsx
@@ -13,6 +13,7 @@ import GameHome from "./GameHome";
       constructor(props){
           super(props)
           this.state = {
+              bordercolor: 'blue',
               clicked: false,
               timerStart: false,
               imgloaded: true,
@@ -36,25 +37,52 @@ import GameHome from "./GameHome";
       }
 
       handleChange(e){
-        this.setState({ [e.target.name]: e.target.value });
+        this.setState({ 
+          [e.target.name]: e.target.value, 
+        });
+
+        // Find Wizard
         if ((this.state.x >= 570 && this.state.x < 585) && (this.state.y >= 256 && this.state.y < 300) && (e.target.value == 'wizard'))
         {
           console.log('wizard found')
           this.setState({
+            bordercolor: 'green',
             wizard: true
           })
         }
+
+        // Find Waldo
+        if ((this.state.x >= 1014 && this.state.x < 1031) && (this.state.y >= 277 && this.state.y < 308) && (e.target.value == 'waldo'))
+        {
+          console.log('wizard found')
+          this.setState({
+            bordercolor: 'green',
+            waldo: true
+          })
+        }
+
+        // Find Odlaw
+        if ((this.state.x >= 362 && this.state.x < 376) && (this.state.y >= 262 && this.state.y < 293) && (e.target.value == 'odlaw'))
+        {
+          console.log('wizard found')
+          this.setState({
+            bordercolor: 'green',
+            odlaw: true
+          })
+        }
+
+
       }
 
       checkBoardClick(e){
-        this.setState({ x: e.clientX, y: e.clientY, clicked: true });
+        this.setState({ x: e.clientX, y: e.clientY, clicked: true, bordercolor: "blue" });
       }
 
 
       render(){
         return(
             <div className='gameContainer'>
-            <BorderPiece top={this.state.y} left={this.state.x} clicked={this.state.clicked ? 'flex' : 'none'} onChange={this.handleChange} value={this.state.selectedChar}></BorderPiece>
+            <BorderPiece top={this.state.y} left={this.state.x} clicked={this.state.clicked ? 'flex' : 'none'} onChange={this.handleChange} value={this.state.selectedChar} characters={{waldo: this.state.waldo, odlaw: this.state.odlaw, wizard: this.state.wizard}} color={this.state.bordercolor}></BorderPiece>
                 <img className='waldoBoard' src={waldoBoard}
                 onLoad={() => this.startTimer()}
                 onClick={this.checkBoardClick}
